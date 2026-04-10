ee860b25a9f12f40fc0d29bdd7b00bd3d86d4814
phatdang <phatdang19052004@gmail.com>
2026-04-10 21:31:47 +0700

    feat(models.enums): add AuditAction enums class

---
+++ src/main/java/org/ping_me/model/enums/AuditAction.java
@@ -0,0 +1,12 @@
+package org.ping_me.model.enums;
+
+/**
+ * @author : user664dntp
+ * @mailto : phatdang19052004@gmail.com
+ * @created : 19/03/2026, Thursday
+ **/
+public enum AuditAction {
+    CREATE,
+    UPDATE,
+    DELETE
+}
