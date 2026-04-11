2e9f175878c4ec3d66b818c6d4ca2685408f8960
phatdang <phatdang19052004@gmail.com>
2026-04-11 12:48:00 +0700

    feat(service): add public interface AdminChatService {  service

---
+++ src/main/java/org/ping_me/service/impl/AdminChatServiceImpl.java
@@ -0,0 +1,67 @@
+package org.ping_me.service.impl;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.ping_me.dto.response.DailyTrendResponse;
+import org.ping_me.model.UserActivityLog;
+import org.ping_me.model.enums.ActivityType;
+import org.ping_me.repository.UserActivityLogRepository;
+import org.ping_me.service.AdminChatService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+
+/**
+ * @author : user664dntp
+ * @mailto : phatdang19052004@gmail.com
+ * @created : 11/04/2026, Saturday
+ **/
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+@Slf4j
+public class AdminChatServiceImpl implements AdminChatService {
+    UserActivityLogRepository repository;
+    MongoTemplate mongoTemplate;
+
+    public long getTotalChatCount(Instant start, Instant end) {
+        return repository.countByTypeAndTimestampBetween(ActivityType.CHAT, start, end);
+    }
+    public Page<UserActivityLog> getRecentChatLogs(Instant start, Instant end, Pageable pageable) {
+        return repository.findByTypeAndTimestampBetween(ActivityType.CHAT, start, end, pageable);
+    }
+
+    public List<DailyTrendResponse> getChatDailyTrend(Instant start, Instant end) {
+        Criteria criteria = Criteria.where("type").is(ActivityType.CHAT)
+                .andOperator(
+                        Criteria.where("timestamp").gte(start),
+                        Criteria.where("timestamp").lte(end)
+                );
+
+        Aggregation aggregation = Aggregation.newAggregation(
+                Aggregation.match(criteria),
+                Aggregation.project()
+                        .andExpression("{$dateToString: {format: '%Y-%m-%d', date: '$timestamp', timezone: 'Asia/Ho_Chi_Minh'}}").as("date"),
+                Aggregation.group("date").count().as("count"),
+                Aggregation.project("count").and("date").previousOperation(),
+                Aggregation.sort(Sort.Direction.ASC, "date")
+        );
+
+        AggregationResults<DailyTrendResponse> results = mongoTemplate.aggregate(
+                aggregation, UserActivityLog.class, DailyTrendResponse.class
+        );
+
+        return results.getMappedResults();
+    }
+}
